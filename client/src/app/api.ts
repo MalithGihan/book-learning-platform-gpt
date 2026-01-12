2321c986068645825b0634966aeaa569772e4260
Malith Gihan <malithgihan000@gmail.com>
2026-01-12 23:28:07 +0530

    fix(frontend): .env backend url path

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -1,17 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = import.meta.env.PRODUCTION_API_BASE_URL as string | undefined;
+const baseUrl = import.meta.env.VITE_API_BASE_URL as string | undefined;
 
 if (!baseUrl) {
-  console.warn("Missing PRODUCTION_API_BASE_URL in client/.env");
+  console.warn("Missing VITE_API_BASE_URL");
 }
 
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: baseUrl ?? "http://localhost:4000/api/v1",
+    baseUrl,
     credentials: "include",
   }),
- tagTypes: ["Courses", "Course", "Enrollments"],
+  tagTypes: ["Courses", "Course", "Enrollments"],
   endpoints: () => ({}),
 });
